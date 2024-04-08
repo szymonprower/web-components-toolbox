65aea97ea7004f1b3df5f4d6a6149f7e4e5a34c4
rs-mgb <roland.szabo@mgb.ch>
2024-04-08 10:21:19 +0200

    Added base mask template

--- src/es/components/prototypes/Validation.js
+++ src/es/components/prototypes/Validation.js
@@ -64,11 +64,79 @@ export const Validation = (ChosenClass = Shadow()) => class Validation extends C
       }
     }
 
-    this.validationPatternInputEventListenerTwo = (event) => {
-      const inputField = event.currentTarget
-      const splittedMaskPattern = this.validationValues[event.currentTarget.getAttribute('name')]['pattern']['mask-value']
-      let splittedInputValue = inputField.value
-      console.log("hu", splittedMaskPattern, splittedInputValue)
+    this.validationPatternInputEventListenerTwo = (e) => {
+      let currentMaskPattern = this.validationValues[e.currentTarget.getAttribute('name')]['pattern']['mask-value']
+      let cursorPos = e.target.selectionStart
+      let currentValue = e.target.value
+      let cleanValue = currentValue.replace(/\D/g, "");
+      //let cleanValue = currentValue
+
+      let formatInput = patternMatch({
+        input: cleanValue,
+        template: currentMaskPattern
+      });
+
+      e.target.value = formatInput
+
+      let isBackspace = (e?.data == null) ? true : false
+      let nextCusPos = nextDigit(formatInput, cursorPos, isBackspace)
+
+      e.currentTarget.setSelectionRange(nextCusPos + 1, nextCusPos + 1);
+    }
+
+    function nextDigit(input, cursorpos, isBackspace) {
+      if (isBackspace) {
+        for (let i = cursorpos - 1; i > 0; i--) {
+          if (/\d/.test(input[i])) {
+            return i
+          }
+        }
+      } else {
+        for (let i = cursorpos - 1; i < input.length; i++) {
+          if (/\d/.test(input[i])) {
+            return i
+          }
+        }
+      }
+
+      return cursorpos
+    }
+
+    function patternMatch({
+      input,
+      template
+    }) {
+      try {
+
+        let j = 0;
+        let plaintext = "";
+        let countj = 0;
+        while (j < template.length) {
+
+          if (countj > input.length - 1) {
+            template = template.substring(0, j);
+            break;
+          }
+
+          if (template[j] == input[j]) {
+            j++;
+            countj++;
+            continue;
+          }
+
+          if (template[j] == "x") {
+            template = template.substring(0, j) + input[countj] + template.substring(j + 1);
+            plaintext = plaintext + input[countj];
+            countj++;
+          }
+          j++;
+        }
+
+        return template
+
+      } catch {
+        return ""
+      }
     }
 
     this.submitFormValidation = (event) => {
